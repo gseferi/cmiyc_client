7a7fdcc3b8ae674c8fb8a982193e8e9769727d7c
Harvey Hyatt <downthedrain97@live.co.uk>
2017-02-23 13:47:55 +0000

    Rejigging audio system

---
+++ src/audio/AudioPlayer.java
@@ -0,0 +1,6 @@
+package audio;
+
+public interface AudioPlayer {
+	public void play(boolean loop);
+	public void stop();
+}
--- src/audio/Sound.java
+++ src/audio/Sound.java
@@ -1,28 +1,40 @@
 package audio;
 
 import java.io.File;
+import java.net.URL;
 
 public enum Sound {
-	FOOTSTEP ("footsteps.wav"), MUSIC_MAIN ("output.midi", AudioType.MIDI), DOOR_OPEN ("open.wav"), BATTERY_LOW ("battery.wav"), WINDOW_BREAK ("window.wav");
+	//FOOTSTEP ("footsteps.wav"), MUSIC_MAIN ("output.midi", AudioType.MIDI), DOOR_OPEN ("open.wav"), BATTERY_LOW ("battery.wav"), WINDOW_BREAK ("window.wav");
+	MUSIC_MAIN ("snow4.mid", AudioType.MIDI);
 	
 	private File audiofile;
-	private AudioType type;
+	private AudioPlayer player;
 
-	private Sound( String filename ) {
+	private Sound(String filename) {
 		this.audiofile = new File(getClass().getResource(filename).getPath());
-		this.type = AudioType.WAV;
+		this.player = new AudioWav(this.audiofile);
 	}
 	
 	private Sound(String filename, AudioType type) {
-		this.audiofile = new File(getClass().getResource(filename).getPath());
-		this.type = type;
+		URL url = getClass().getResource(filename);
+		this.audiofile = new File(url.getPath());
+		switch(type) {
+		case MIDI:
+			this.player = new AudioMidi(this.audiofile);
+		case WAV:
+			this.player = new AudioWav(this.audiofile);
+		}
 	}
 
-	public File getAudiofile() {
-		return this.audiofile;
+	public void play() {
+		this.player.play(false);
 	}
 	
-	public AudioType getType() {
-		return type;
+	public void playLoop() {
+		this.player.stop();
+	}
+	
+	public void stop() {
+		this.player.stop();
 	}
 }
