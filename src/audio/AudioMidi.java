741721be08b81e6337590be739ffd4cb545e300e
Harvey Hyatt <downthedrain97@live.co.uk>
2017-02-28 14:05:47 +0000

    bugfix

--- src/audio/AudioMidi.java
+++ src/audio/AudioMidi.java
@@ -20,16 +20,12 @@ public class AudioMidi implements AudioPlayer {
 	public AudioMidi(File audioFile) {
 		running = false;
 		this.audioFile = audioFile;
-		try {
-			sequencer = MidiSystem.getSequencer();
-		} catch (MidiUnavailableException e) {
-			e.printStackTrace();
-		}
 	}
 	
 	private void setup() {
 		running = true;
 		try {
+			sequencer = MidiSystem.getSequencer();
 			sequencer.open();
 		} catch (MidiUnavailableException e) {
 			e.printStackTrace();
