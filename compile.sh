2297dea329da85e40ae5557d74e107cb8f646ee6
Paul Sandbrook <pes534@cs.bham.ac.uk>
2017-03-01 21:50:58 +0000

    Add JUnit support.

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf bin
 mkdir -p bin
-find src ../cmiyc_shared/src -name '*.java' -exec javac -d bin {} +
+find src ../cmiyc_shared/src -name '*.java' -exec javac -d bin -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar {} +
